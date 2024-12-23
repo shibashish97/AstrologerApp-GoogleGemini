65cb4ea0ae468f9c686ba1ef27f78a3f81f5a8e8
Shibashish Nayak <shibashishnayak97@gmail.com>
2024-12-23 17:28:13 +0530

    Update astrologer.py

--- astrologer.py
+++ astrologer.py
@@ -10,7 +10,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 # Function to load Google Gemini Pro Vision API and get response
 def get_gemini_response(input, image, prompt):
-    model = genai.GenerativeModel('gemini-pro-vision')
+    model = genai.GenerativeModel('gemini-1.5-flash')
     response = model.generate_content([input, image[0], prompt])
     return response.text
 
